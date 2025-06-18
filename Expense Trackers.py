ffb8fcfbf5ce0f8fde6f901f869c6e66afada164
Bluey <BlueyVuitton713@gmail.com>
2025-06-17 17:44:45 -0700

    updated the choice and enter functions. Still need work

--- Expense Trackers.py
+++ Expense Trackers.py
@@ -10,34 +10,73 @@
 #store expenses in a dictionary with categories as keys and lists of amounts as values
 #provide options to view total spent accross all categories, list unique categories, show expenses above a user specified amount
 
-expenses = {"Food": {"Groceries": 0, "Fast Food": 0},
+import sys
+expense_list = ["Food", "Gas", "Insurance", "Property Tax", "Utilities"] #"Groceries", "Fast Food"
+#Still need to thing about how to incorporate lists. So far im just using a giant dictionary
+expenses_dict = {"Food": {"Groceries": 0, "Fast Food": 0},
             "Gas": 0,
             "Phone/Internet": 0,
             "Utilities": {"Gas":0, "Electricity": 0, "Water": 0},
-
              }
+
+running = True
+
+#Functions
+
 def user_choice():
     #This is the starting point of the whole loop. Will take user input to see if the want to add or view
-    choice = int(input("What would you like to do? 1.Enter Expense 2.View Totals: "))
-    if choice == 1:
-        enter_expenses()
-    else:
-        view_totals()
+    try:
+        choice = int(input("What would you like to do? 1.Enter Expense 2.View Totals 3.Quit: "))
+        if choice == 1:
+            enter_expenses()
+        elif choice == 2:
+            view_totals()
+        elif choice ==3:
+            print("See you next time!")
+            return False
+    except:
+        print("You didn't enter a valid response. Try again.")
+        return True
+
 
 def enter_expenses():
     #This will be the main function to take the user input and properly allocate expenses
-    expense_entry = int(input("Which category would you like to enter? 1.Food 2.Gas 3.Phone/Internet 4.Utilities: "))
-    if expense_entry == 1:
-        sub_category = input("This category has selectable sub-categories. Which")
-        print(f"This category has sub-categories")
-        amount = float(input("Enter the "))
-        #Need to figure out how to give a value to a nested dictionary
+    expense_entry = input("Which category would you like to enter? 1.Food 2.Gas 3.Phone/Internet 4.Utilities: ").strip().title()
+    # ^^ Thinking of changing this to strings to use them as the "key" ^^ and maybe have doulbe input. have if statment check string or int?
+    if expense_entry == "Food":
+        for key in expenses_dict["Food"]:
+            print(key)
+        #Thinking of adding a loop to check if the key selected includes a value or if its type(dict) then to print out all keys
+        sub_category = input("This category has the stated selectable sub-categories. Which do you want to access: ").strip().title()
+        if sub_category == "Groceries":
+            amount = float(input("Enter the amount of the expense for Groceries: ")) #maybe switch these to f strings and include the key
+            expenses_dict[expense_entry][sub_category] += amount
+            print(f"The updated value of {sub_category} is: {expenses_dict[expense_entry][sub_category]}")
+            return True
+
+        elif sub_category == "Fast Food":
+            amount = float(input("Enter the amount of the expense for Fast Food: "))
+            expenses_dict[expense_entry][sub_category] += amount
+            print(f"The updated value of {sub_category} is: {expenses_dict[expense_entry][sub_category]}")
+            # return True
+            #^^ Need to figure out how to make the program keep looping after the print statement. Tried adding return True as a test but no.
     elif expense_entry == 2:
+        #If 2 and no sub-category
+        #input from user as float
+        #expenses[
+        #
         pass
     elif expense_entry == 3:
         pass
     elif expense_entry == 4:
         pass
+
+def continue_entering():
+    pass
+    #is this function required to keep the program running?
+    #was thinking of having additional print statements asking to continue or print out stuff which would just call the previous functions
+
+
 
 def view_totals():
     #Will use this to ask user which expense account or sub account the want to view
@@ -48,3 +87,8 @@ def total_threshold():
     #Will use to take user input and search for expense totals exceeding or equal to dependin on
     #user input
     pass
+
+#Actual program running
+while running:
+    running = user_choice()
+
