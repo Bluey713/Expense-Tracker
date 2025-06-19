9b9491c40ec0748c1e31aa81a5c2905136810801
Bluey <BlueyVuitton713@gmail.com>
2025-06-18 17:32:09 -0700

    Started working on returning totals. works the same as expenses but need another statement to return totals instead of just sub totals.

--- Expense Trackers.py
+++ Expense Trackers.py
@@ -3,30 +3,29 @@
 #basic insights(total spent, unique categories, or expenses above a threshold)
 
 #lists: store expense entries
-#Dictionaries: map expenses to categories and amounts
+#Dictionaries: map expenses to categories and amounts - This part is done but user cant add new accounts
 #set: find unique categories
 #requirements:
-#allow user to input expenses
+#allow user to input expenses - This part is done but limited to existing expense accounts
 #store expenses in a dictionary with categories as keys and lists of amounts as values
 #provide options to view total spent accross all categories, list unique categories, show expenses above a user specified amount
 
 #Functions should only have one task, they should not be asking for input
+
+
 
 #import sys
 expense_list = ["Food", "Gas", "Insurance", "Property Tax", "Utilities"] #"Groceries", "Fast Food"
 #Still need to thing about how to incorporate lists. So far im just using a giant dictionary
 
 
-
-
 #Functions
 
-def user_choice():
-    #This is the starting point of the whole loop. Will take user input to see if the want to add or view
+def user_choice(): #Not used
+    # This is the starting point of the whole loop. Will take user input to see if the want to add or view
     try:
         choice = int(input("What would you like to do? 1.Enter Expense 2.View Totals 3.Quit: "))
         if choice == 1:
-
             enter_expenses()
         elif choice == 2:
             view_totals()
@@ -36,6 +35,7 @@ def user_choice():
     except:
         print("You didn't enter a valid response. Try again.")
         return True
+#Decided to scrap this functions and just use the block of code inside the while loop instead. Had an issue break out of the loop this way
 
 
 def enter_expenses(expense_entry, expenses_dict):
@@ -90,11 +90,41 @@ def continue_entering():
     #was thinking of having additional print statements asking to continue or print out stuff which would just call the previous functions
 
 
+def view_totals(user_view):
+    #Will use this to take user input on which expense account or sub account the want to view
+    if user_view == "Food":
+        for key in expenses_dict[user_view]:
+            print(key)
+        # Thinking of adding a loop to check if the key selected includes a value or if its type(dict) then to print out all keys
+        sub_category = input("This category has the stated selectable sub-categories. Which do you want to access: ").strip().title()
+        #Need to add another if statement that takes the sub cat input and if they want to see the total not justs a specific sub cat
+        #it prints the dict key value sum. sum(expenses_dict[sub_catgory]) something like that
+        if sub_category == "Groceries":
+            print(f"The current value of {sub_category} is: {expenses_dict[expense_entry][sub_category]}")
+        elif sub_category == "Fast Food":
+            print(f"The current value of {sub_category} is: {expenses_dict[expense_entry][sub_category]}")
 
-def view_totals():
-    #Will use this to ask user which expense account or sub account the want to view
-    user_view = input("Which total would you like to view: ")
-    pass
+    elif expense_entry == "Gas":
+        print(f"The current value of {expense_entry} is: {expenses_dict[expense_entry]}")
+
+    elif expense_entry == "Phone/Internet":
+        print(f"The current value of {expense_entry} is: {expenses_dict[expense_entry]}")
+
+    elif expense_entry == "Utilities":
+        for key in expenses_dict[expense_entry]:
+            print(key)
+        #Thinking of adding a loop to check if the key selected includes a value or if its type(dict) then to print out all keys
+        sub_category = input("This category has the stated selectable sub-categories. Which do you want to access: ").strip().title()
+        if sub_category == "Gas":
+            print(f"The current value of {sub_category} is: {expenses_dict[expense_entry][sub_category]}")
+
+        if sub_category == "Electricity":
+            print(f"The current value of {sub_category} is: {expenses_dict[expense_entry][sub_category]}")
+
+        if sub_category == "Water":
+            print(f"The current value of {sub_category} is: {expenses_dict[expense_entry][sub_category]}")
+
+    return expense_entry
 
 def total_threshold():
     #Will use to take user input and search for expense totals exceeding or equal to dependin on
@@ -112,13 +142,14 @@ running = True
 
 while running:
     try:
-        choice = int(input("What would you like to do? 1.Enter Expense 2.View Totals 3.Print 4.Quit: "))
+        choice = int(input("What would you like to do? 1.Enter Expense 2.View Totals 3.Print Listing 4.Quit: "))
         if choice == 1:
             expense_entry = input("Which category would you like to enter? 1.Food 2.Gas 3.Phone/Internet 4.Utilities: ").strip().title()
             # ^^ Thinking of changing this to strings to use them as the "key" ^^ and maybe have doulbe input. have if statment check string or int?
             enter_expenses(expense_entry, expenses_dict)
         elif choice == 2:
-            view_totals()
+            user_view = input("Which category would you like to view? 1.Food 2.Gas 3.Phone/Internet 4.Utilities: ").strip().title()
+            view_totals(user_view)
         elif choice == 3:
             print(expenses_dict)
         elif choice == 4:
